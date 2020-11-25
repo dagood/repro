38015db61b90ffca65cd09550f16a3a7c06828c1
Davis Goodin <dagood@microsoft.com>
2020-11-25 01:17:53 -0600

    Use '-out=', not '>'

--- repro.sh
+++ repro.sh
@@ -1,13 +1,13 @@
 set -ex
 
 # Set up repro based on DLL to make it more realistic.
-./coreclr-tools/ildasm Repro.dll > repro.il
+./coreclr-tools/ildasm Repro.dll -out=repro.il
 
 # Take the IL and make a DLL: round-tripped.
 ./coreclr-tools/ilasm repro.il -dll -output=RoundTripped.dll
 
 # In order to compare, disassemble the roundtripped DLL again.
-./coreclr-tools/ildasm RoundTripped.dll > repro-after-roundtrip.il
+./coreclr-tools/ildasm RoundTripped.dll -out=repro-after-roundtrip.il
 
 # Compare the IL that's been built by ilasm vs. the original IL produced by ildasm.
 git diff --no-index repro.il repro-after-roundtrip.il
